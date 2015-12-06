c0f46e153da7f2a4408859569b1e351ac22e121f
David Cardozo <gd.cardozo684@uniandes.edu.co>
2015-12-05 23:35:33 -0500

    Adding Problem 3, getting to know iterators

--- Problem3GreatestFactor.py
+++ Problem3GreatestFactor.py
@@ -1,9 +1,25 @@
 import math
-factor = []
 
-for i in range(int(math.sqrt(600851475143))):
-    if i != 0:
-        if 600851475143 % i  == 0:
-            factor.append(i)
 
-print(factor)
+def prime_factors(n):
+    """
+    Takes a positive integer, and returns its list of prime divisors
+    :rtype: list of prime factors
+    """
+    divisor = []
+    bound = int(math.sqrt(n))
+    for i in range(bound):
+        if i != 0:
+            if n % i == 0:
+                print(i)
+                divisor.append(i)
+    print(divisor)
+    for d in divisor:
+        for f in divisor:
+            if f != d and f % d == 0 and d != 1:
+                divisor.remove(f)
+    return divisor
+
+
+x = prime_factors(600851475143)
+print(x)
